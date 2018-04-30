d4dbd519154e1c121866a070ce07c831c6eba7f6
Evandro P. santos <evandro.santos@multilaser.com.br>
2018-04-30 15:34:25 -0300

    Implementando timeout em Promises

--- public/app/app.js
+++ public/app/app.js
@@ -1,21 +1,24 @@
-import { log } from './utils/promise-helpers.js';
+import { log, timeoutPromise } from './utils/promise-helpers.js';
 import './utils/array-helpers.js';
 import { notasService as service } from './nota/service.js'; 
-import { takeUntil, debounceTime } from './utils/operators.js';
+import { takeUntil, debounceTime, partialize, pipe } from './utils/operators.js';
 
 // import { filteredNumbers, newNumbers, sum } from './tests/cap02.js';
 // import { ehDivisivel, ehDivisivelPorDois } from './tests/cap03.js';
 // import './tests/cap04.js';
+// import './tests/cap05.js';
 
-const operation1 = takeUntil(3, () => 
-    service
-    .sumItems('2143')
+const operations = pipe(
+    partialize(takeUntil, 3),
+    partialize(debounceTime, 500),
+);
+
+const action = operations( () => 
+    timeoutPromise( 200, service.sumItems('2143') )
     .then(console.log)
     .catch(console.log)
 );
 
-const operation2 = debounceTime(500, operation1);
-
-document.querySelector('#myButton').onclick = operation2;
+document.querySelector('#myButton').onclick = action;
     
 
---
+++ public/app/tests/cap05.js
@@ -0,0 +1,11 @@
+const promise1 = new Promise( (resolve, reject) => 
+    setTimeout( () => resolve('promise 1 resolved'), 3000 )
+);
+
+const promise2 = new Promise( (resolve, reject) => 
+    setTimeout( () => resolve('promise 2 resolved'), 1000)
+);
+
+Promise.race( [promise1, promise2] )
+.then(console.log)
+.catch(console.log);
