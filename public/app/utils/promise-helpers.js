99914e2c657032d1de5b41916c88261aa6d3129d
Evandro P. santos <evandro.santos@multilaser.com.br>
2018-05-04 16:23:14 -0300

    Implementando retry em Promises

--- public/app/utils/promise-helpers.js
+++ public/app/utils/promise-helpers.js
@@ -6,22 +6,25 @@ export const log = param => {
 }
 
 export const timeoutPromise = (milliseconds, promise) => {
-
+    
     const timeout =  new Promise((resolve, reject) =>
-        setTimeout(() => 
-            reject(`Limite da operação excedido (limite: ${milliseconds} ms)`), milliseconds)
-        );
+    setTimeout(() => 
+    reject(`Limite da operação excedido (limite: ${milliseconds} ms)`), milliseconds)
+);
 
-    return Promise.race( [timeout, promise] );
+return Promise.race( [timeout, promise] );
 };
 
 export const delay = milliseconds => data =>
     new Promise( (resolve, reject) => 
         setTimeout( () => resolve(data), milliseconds) );
 
-export const retry = (retries, milliseconds, fn) => 
-    fn().catch( err => {
+export const retry = (retries, milliseconds, fn) =>
+    
+    fn().catch(err => {
         console.log(retries);
-
-        return retries > 1 ? retry(retries - 1, milliseconds, fn) : Promise.reject(err);
-    });
+    
+        return delay(milliseconds)().then(() => 
+            retries > 1 ? retry(retries - 1, milliseconds, fn) : Promise.reject(err))
+    }
+);
