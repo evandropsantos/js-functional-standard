29038bcef6872dd7ed0acd05e418578bf6010ea8
Evandro P. santos <evandro.santos@multilaser.com.br>
2018-05-08 09:15:19 -0300

    Pattern Publisher/Subscriber

--- public/app/app.js
+++ public/app/app.js
@@ -1,24 +1,18 @@
 import { log, timeoutPromise, retry } from './utils/promise-helpers.js';
 import './utils/array-helpers.js';
-import { notasService as service } from './nota/service.js'; 
+import { notasService as service } from './nota/service.js';
 import { takeUntil, debounceTime, partialize, pipe } from './utils/operators.js';
-
-// import { filteredNumbers, newNumbers, sum } from './tests/cap02.js';
-// import { ehDivisivel, ehDivisivelPorDois } from './tests/cap03.js';
-// import './tests/cap04.js';
-import './tests/cap05.js';
+import { EventEmitter } from './utils/event-emitter.js';
 
 const operations = pipe(
     partialize(takeUntil, 3),
-    partialize(debounceTime, 500),
+    partialize(debounceTime, 500)
 );
 
-const action = operations( () => 
-    retry(3, 3000, () => timeoutPromise(200, service.sumItems('2143')))
-    .then(console.log)
+const action = operations(() => 
+    retry( 3, 3000, () => timeoutPromise( 200, service.sumItems('2143') ) )
+    .then( total => EventEmitter.emit('itensTotalizados', total) )
     .catch(console.log)
 );
 
 document.querySelector('#myButton').onclick = action;
-    
-
